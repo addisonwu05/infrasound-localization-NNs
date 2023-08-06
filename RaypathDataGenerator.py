20d1b5c5a4c0068fd5d92088b731d81474f9c3cc
Addison Wu <addisonwu@princeton.edu>
2023-08-06 16:21:55 +0200

    changed file formats

---
+++ RaypathDataGenerator.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+import numpy as np
+
+#Get the cursor at the right place
+time.sleep(10)
+print(pyautogui.position())
+
+#First, select the atmospheric profile
+for i in range(0, 17):
+    atmo = "example" + str(i) + ".met"
+    #Then, set an initial source altitude
+    for j in np.arange(0, 20, 0.05):
+        #ROUND J TO 2 DECIMAL PLACES SO I DON'T GET BAD LONG FLOATING POINT ARITHMETIC
+        j = np.round(j, decimals=2)
+        #Then, run the command
+        pyautogui.typewrite("./infraGA/bin/infraga-2d -prop ./infraGA/training_data/profs/" + atmo + " src_alt=" + str(j) + "incl_min=-89" + " incl_max=89")
+        pyautogui.press("enter")
+        #Rename and move the file
+        newFileName = "Atmo" + str(i) + "_Alt" + str(j) + ".raypaths.dat"
+        pyautogui.typewrite("mv ./infraGA/training_data/profs/example" + str(i) + ".raypaths.dat" + " ./raw_raypaths/" + newFileName)
+        pyautogui.press("enter")
+
---
+++ RaypathDataScraper.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import pandas as pd
+
+startPath = "./raw_raypaths"
+endPath = "./scraped_raypaths"
+
+for filename in os.listdir(startPath):
+    #Set the altitude of the microphone
+    microphone_alt = np.around(np.random.uniform(3, 20), decimals=2)
+    microphone_r = np.around(np.random.uniform(10, 900), decimals=3)
+
+    #Read datafile and ensure it is float file
+    df = pd.read_table(os.path.join(startPath, filename), dtype=float)
+    df = df.astype(float)
+
+    #parse for relevant data, r and z must be within the microphone's pickup range
+    #Spherical distribution? Pythagorean inequality
+    drop_row_list = []
+    for i in range(0, len(df)):
+        if (microphone_r - float(df["# r [km]"][i]))**2 + (microphone_alt - float(df["z [km]"][i]))**2 > 2.5**2:  #microphone with capture radius of 2.5 km
+            drop_row_list.append(i) # data is out of range of microphone discard it
+    df = df.drop(labels = drop_row_list, axis = 0) #discard said rows
+
+    #now we need to adjust the time column so that it's scaled down (we can only use what we know)
+    #the times are set relative to when the microphone registers, not how long it takes after emitted from source
+    #drop irrelevant columns
+    df["time [s]"] = df["time [s]"] - min(df["time [s]"])
+    df = df.drop(columns=["r [km]", "z [km]", "trans. coeff. [dB]"])
+
+    #write and save to a new file name
+    newfile = f"MicAlt_{microphone_alt}_MicDistance_{microphone_r}_{filename}"
+
+    #save to the scraped folder as a TSV (tab CSV)
+    output_file_path = os.path.join(endPath, newfile)
+    df.to_csv(output_file_path, sep='\t', index=False, header=False)
+
