9820545e8da348f38258af68213fcee1cf196b9f
Addison Wu <addisonwu@princeton.edu>
2023-08-08 15:09:57 +0200

    fixed spacing

--- RaypathDataGenerator.py
+++ RaypathDataGenerator.py
@@ -9,7 +9,7 @@ for i in range(0, 17):
         #ROUND J TO 2 DECIMAL PLACES SO I DON'T GET BAD LONG FLOATING POINT ARITHMETIC
         j = np.round(j, decimals=2)
         #Then, run the command
-        os.system("./infraGA/bin/infraga-2d -prop ./infraGA/examples/profs/" + atmo + " src_alt=" + str(j) + "incl_min=-89" + " incl_max=89")
+        os.system("./infraGA/bin/infraga-2d -prop ./infraGA/examples/profs/" + atmo + " src_alt=" + str(j) + " incl_min=-89 " + " incl_max=89")
         #Rename and move the file
         newFileName = "Atmo" + str(i) + "_Alt" + str(j) + ".raypaths.dat"
         os.system("mv ./infraGA/examples/profs/example" + str(i) + ".raypaths.dat" + " ./raw_raypaths/" + newFileName)
