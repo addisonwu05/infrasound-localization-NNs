c9709caf26a65b273672ac600a6edbe8c786b50a
Addison Wu <addisonwu@princeton.edu>
2023-08-12 17:52:39 -0400

    added attenuation converter file

---
+++ AttenuationtoRelativeAmplitude.py
@@ -0,0 +1,3 @@
+#this code is intended to convert the A_Atmo (atmospheric attenuation) [dB] column to a series of relative SPL measurements wrt time
+
+#will then hopefully be used to make a FFT vector
