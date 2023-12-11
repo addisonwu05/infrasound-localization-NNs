86e2c7ae4a033ec6d336e9997151c3c58232d009
Addison Wu <addisonwu@princeton.edu>
2023-12-11 09:13:52 -0500

    debugging

--- DeleteShortCSV.py
+++ DeleteShortCSV.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 #Delete scraped raypath files that are too small 
 path = "./CAT_Infrasound_Data/scraped_raypaths"
+count = 0
 for filename in os.listdir(path):
+    count = count + 1
     df = pd.read_csv(os.path.join(path, filename))
     print(len(df))
     if len(df) < 10: #change at discretion
         os.remove(os.path.join(os.path.join(path, filename)))
         print("deleted")
+print(count)
