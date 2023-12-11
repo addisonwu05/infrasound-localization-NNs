74967f35b814d04bae64f5f00c470799e188e10d
Addison Wu <addisonwu@princeton.edu>
2023-12-11 09:02:55 -0500

    added debugging

--- DeleteShortCSV.py
+++ DeleteShortCSV.py
@@ -7,3 +7,4 @@ for filename in os.listdir(path):
     df = pd.read_csv(os.path.join(path, filename))
     if len(df) < 10: #change at discretion
         os.remove(os.path.join(os.path.join(path, filename)))
+        print("deleted")
