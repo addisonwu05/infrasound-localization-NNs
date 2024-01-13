af24bbda005c8d57857a77b5d9378e09a439bdfd
Addison Wu <addisonwu@princeton.edu>
2024-01-12 19:16:49 -0500

    folders instead of files

--- DeleteFiles.py
+++ DeleteFiles.py
@@ -1,20 +1,21 @@
 import os
 import random
+import shutil
 
-# Path to the folder where files will be deleted
+# Path to the folder where subfolders will be deleted
 folder_path = "./CAT_Infrasound_Data/scraped_data"  # Replace with your folder path
 
-# Number of files to delete
-number_of_files_to_delete = 30000
+# Number of subfolders to delete
+number_of_subfolders_to_delete = 30000
 
-# List all files in the directory
-files = [file for file in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, file))]
+# List all subfolders in the directory
+subfolders = [os.path.join(folder_path, folder) for folder in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, folder))]
 
-# Randomly select files to delete
-files_to_delete = random.sample(files, min(number_of_files_to_delete, len(files)))
+# Randomly select subfolders to delete
+subfolders_to_delete = random.sample(subfolders, min(number_of_subfolders_to_delete, len(subfolders)))
 
-# Delete the selected files
-for file in files_to_delete:
-    os.remove(os.path.join(folder_path, file))
+# Delete the selected subfolders
+for subfolder in subfolders_to_delete:
+    shutil.rmtree(subfolder)
 
-print(f"Deleted {len(files_to_delete)} files.")
+print(f"Deleted {len(subfolders_to_delete)} subfolders.")
